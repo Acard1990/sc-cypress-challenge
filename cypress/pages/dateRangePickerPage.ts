b9ff3a95155102e96445f634911373237facacc3
Scott Cardinali <scott.cardinali@gmail.com>
2024-10-15 20:05:38 -0400

    updating test cases

--- cypress/pages/dateRangePickerPage.ts
+++ cypress/pages/dateRangePickerPage.ts
@@ -1,50 +1,81 @@
 import DateRangePickerSelectors from '../selectors/dateRangePickerSelectors';
 
 class DateRangePickerPage {
-    navToDateRangePickerPage() {
-        cy.visit('/');
-        DateRangePickerSelectors.dateRangeSelect.should('be.visible');
-    }
+  navToDateRangePickerPage() {
+    cy.visit('/');
+    DateRangePickerSelectors.dateRangeSelect.should('be.visible');
+  }
 
-    assertStartTimePlaceHolder() {
-        DateRangePickerSelectors.startDateInputField
-            .should('be.visible')
-            .and('have.attr', 'placeholder', 'mm/dd/yyyy');  
-    }
+  assertStartTimePlaceHolder() {
+    DateRangePickerSelectors.startDateInputField
+      .should('be.visible')
+      .and('have.attr', 'placeholder', 'mm/dd/yyyy');
+  }
 
-    assertEndTimePlaceHolder() {
-        DateRangePickerSelectors.endDateInputField
-            .should('be.visible')
-            .and('have.attr', 'placeholder', 'mm/dd/yyyy');  
-    }
+  assertEndTimePlaceHolder() {
+    DateRangePickerSelectors.endDateInputField
+      .should('be.visible')
+      .and('have.attr', 'placeholder', 'mm/dd/yyyy');
+  }
 
-    clickStartDateInputField() {
-        DateRangePickerSelectors.startDateInputField.click();
-    }
+  clickStartDateInputField() {
+    DateRangePickerSelectors.startDateInputField.click();
+  }
 
-    clickEndDateInputField() {
-        DateRangePickerSelectors.endDateInputField.click();
-    }
+  clickEndDateInputField() {
+    DateRangePickerSelectors.endDateInputField.click();
+  }
 
-    assertModalIsVisible() {
-        DateRangePickerSelectors.datePickerModal.should('be.visible');
-    }
+  assertModalIsVisible() {
+    DateRangePickerSelectors.datePickerModal.should('be.visible');
+  }
 
-    clickSpecifiedDay(DAY: string) {
-        cy.contains(DAY).click();
-    }
+  assertModalIsNotVisible() {
+    DateRangePickerSelectors.datePickerModal.should('not.be.visible');
+  }
 
-    assertStartInputFieldVal(expectedVal: string) {
-        DateRangePickerSelectors.startDateInputField.should('have.value', expectedVal);
-    }
+  clickSpecifiedDay(DAY: string) {
+    cy.contains(DAY).click();
+  }
 
-    assertEndInputFieldVal(expectedVal: string) {
-        DateRangePickerSelectors.endDateInputField.should('have.value', expectedVal);
-    }
+  assertStartInputFieldVal(expectedVal: string) {
+    DateRangePickerSelectors.startDateInputField.should(
+      'have.value',
+      expectedVal
+    );
+  }
 
-    assertEndInputFieldValIsNot(expectedVal: string) {
-        DateRangePickerSelectors.endDateInputField.should('not.have.value', expectedVal);
-    }
+  assertEndInputFieldVal(expectedVal: string) {
+    DateRangePickerSelectors.endDateInputField.should(
+      'have.value',
+      expectedVal
+    );
+  }
+
+  assertEndInputFieldValIsNot(expectedVal: string) {
+    DateRangePickerSelectors.endDateInputField.should(
+      'not.have.value',
+      expectedVal
+    );
+  }
+
+  typeValToInput(value) {
+    DateRangePickerSelectors.startDateInputField.type(value);
+  }
+
+  clickNextMonthBtn() {
+    DateRangePickerSelectors.nextMonthBtn.click();
+  }
+
+  clickPreviousMonthBtn() {
+    DateRangePickerSelectors.previousMonthBtn.click();
+  }
+
+  assertMonthModalHeaderValue(expectedVal: string) {
+    DateRangePickerSelectors.monthSpan
+      .contains(expectedVal)
+      .should('be.visible');
+  }
 }
 
 export default DateRangePickerPage;
--- cypress/selectors/dateRangePickerSelectors.ts
+++ cypress/selectors/dateRangePickerSelectors.ts
@@ -1,20 +1,30 @@
 class DateRangePickerSelectors {
-    
-    get dateRangeSelect() {
-        return cy.get('.DateRangeSelect')
-    }
-    get startDateInputField() {
-        return cy.get('#mui-4')
-    }
+  get dateRangeSelect() {
+    return cy.get('.DateRangeSelect');
+  }
+  get startDateInputField() {
+    return cy.get('#mui-4');
+  }
 
-    get endDateInputField() {
-        return cy.get('#mui-5')
-    }
+  get endDateInputField() {
+    return cy.get('#mui-5');
+  }
 
-    get datePickerModal() {
-        return cy.get('body > div:nth-of-type(2)')
-    }
+  get datePickerModal() {
+    return cy.get('body > div:nth-of-type(2)');
+  }
 
+  get nextMonthBtn() {
+    return cy.get(':nth-child(2) > .css-f7iyql > .MuiIconButton-edgeStart');
+  }
+
+  get previousMonthBtn() {
+    return cy.get(':nth-child(1) > .css-f7iyql > .MuiIconButton-edgeEnd');
+  }
+
+  get monthSpan() {
+    return cy.get('span');
+  }
 }
 
-export default new DateRangePickerSelectors()
+export default new DateRangePickerSelectors();
